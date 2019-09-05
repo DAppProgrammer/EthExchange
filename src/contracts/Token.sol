c863cfc2b2a67f1900d736e7e3d9721b94f14f39
DappProgrammer <DappProgrammer@gmail.com>
2019-09-05 19:18:06 +0530

    transfer method, event and test cases

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -1,13 +1,30 @@
 pragma solidity ^0.5.0;
 
+import "openzeppelin-solidity/contracts/math/safeMath.sol";
 
 contract Token {
+  using SafeMath for uint;
+
+  //variables
   string public name = 'Dapp Token';
   string public symbol = 'DAPP';
   uint256 public decimals = 18;
   uint256 public totalSupply;
-
+  mapping(address => uint256) public balanceOf;
   constructor() public {
     totalSupply = 1000000 * (10 ** decimals);
+    balanceOf[msg.sender] = totalSupply;
+  }
+  /* Events */
+  event Transfer(address indexed from, address indexed to, uint256 value);
+  /* Send tokens */
+  function transfer(address _to, uint256 _value) public returns (bool success) {
+    require(_to != address(0),'Invalid recipient address');
+    require(balanceOf[msg.sender] >= _value,'Do not have enough balance to transfer');
+
+    balanceOf[msg.sender] = balanceOf[msg.sender].sub(_value);
+    balanceOf[_to] = balanceOf[_to].add(_value);
+    emit Transfer(msg.sender, _to, _value);
+    return true;
   }
 }
--- test/token.js
+++ test/token.js
@@ -1,21 +1,25 @@
+import { tokens, EVM_REVERT } from "./helpers";
+import { AssertionError } from "assert";
+import { async } from "q";
+
 var Token = artifacts.require("Token");
 
 require("chai")
   .use(require("chai-as-promised"))
   .should();
 
-contract("Token", function(accounts) {
+contract("Token", function([deployer, sender, receiver]) {
   let token;
   const name = "Dapp Token";
   const symbol = "DAPP";
   const decimals = "18";
-  const totalSupply = "1000000000000000000000000";
+  const totalSupply = tokens(1000000).toString();
 
   beforeEach(async () => {
     token = await Token.deployed();
   });
 
-  describe("deployment", () => {
+  describe("deployment", async () => {
     it("tracks the name", async () => {
       const result = await token.name();
       //assert.equal(name, "My Token", 'name should be "My Token"');
@@ -40,6 +44,67 @@ contract("Token", function(accounts) {
           totalSupply,
           "Total supply should be equal '1000000000000000000000000'"
         );
+    });
+
+    it("assigns the total supply to the deployer", async () => {
+      const result = await token.balanceOf(deployer);
+      result
+        .toString()
+        .should.equal(
+          totalSupply,
+          "assigns total supply to deployer correctly"
+        );
+    });
+  });
+
+  describe("sending tokens", async () => {
+    describe("success", async () => {
+      let result;
+      let amount;
+      beforeEach(async () => {
+        amount = tokens(100);
+        result = await token.transfer(receiver, tokens(100).toString(), {
+          from: deployer
+        });
+      });
+
+      it("transfers token balances", async () => {
+        let balanceOf;
+        balanceOf = await token.balanceOf(deployer);
+        balanceOf.toString().should.equal(tokens(999900).toString());
+
+        balanceOf = await token.balanceOf(receiver);
+        balanceOf.toString().should.equal(amount.toString());
+      });
+
+      it("emits a transfer event", async () => {
+        const log = result.logs[0];
+        log.event.should.equal("Transfer");
+        const event = log.args;
+        event.from.toString().should.equal(deployer, "from addres is correct");
+        event.to.toString().should.equal(receiver, "to addres is correct");
+        event.value
+          .toString()
+          .should.equal(tokens(100).toString(), "value is correct");
+      });
+    });
+    describe("failure", async () => {
+      it("rejects insufficient balances", async () => {
+        let invalidAmount;
+        invalidAmount = tokens(1000000000);
+        await token
+          .transfer(receiver, invalidAmount, { from: deployer })
+          .should.be.rejectedWith(EVM_REVERT);
+
+        invalidAmount = tokens(1000);
+        await token
+          .transfer(deployer, invalidAmount, { from: receiver })
+          .should.be.rejectedWith(EVM_REVERT);
+      });
+
+      it("rejects invalid recipient", async () => {
+        await token.transfer(0x0, 10, { from: deployer }).should.be.rejected;
+      });
     });
   });
 });
