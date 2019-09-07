ec020de1cdcad88c4d4e64369c356b04d1252503
DappProgrammer <DappProgrammer@gmail.com>
2019-09-07 16:20:48 +0530

    Exchange file removed from master branch

--- test/exchange.js
+++
@@ -1,9 +0,0 @@
-var Exchange = artifacts.require("Exchange");
-
-contract("Exchange", function(accounts) {
-  it("should assert true", function(done) {
-    var exchange = Exchange.deployed();
-    assert.isTrue(true);
-    done();
-  });
-});
